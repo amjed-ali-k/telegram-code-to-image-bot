d5ae78508fd81145b08b2b3be8d94b1cb4c5a434
amjed-ali-k <amjedmgm@gmail.com>
2023-09-23 20:17:16 +0530

    ✨ feat(*): Updated functions
    Add font
    Add logger
    Add listner
    and more.

--- index.js
+++ index.js
@@ -2,21 +2,62 @@ import { Input, Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import dotenv from "dotenv";
 import { generateCodeImage } from "./lib/carbon.js";
+import { getImage } from "./lib/mods.js";
+import pino from "pino";
+import pretty from "pino-pretty";
+
+const logger = pino(
+  pretty({
+    colorize: true,
+    colorizeObjects: true,
+  })
+);
 
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.start((ctx) => ctx.reply("Welcome to Code Beautifier Bot"));
+bot.help((ctx) => ctx.reply("Send me a code to beautify"));
 
-bot.command("format", (ctx) => {
-  const msg = ctx.message.text.replace("/format", "");
-  generateCodeImage(msg).then((image) => {
-    ctx.replyWithPhoto(Input.fromBuffer(image, "code.png"), {
-      caption: "Formatted 🎉",
-    });
+bot.on(message("text"), async (ctx) => {
+  const botUserName = "@" + bot.botInfo.username;
+  if (ctx.message.chat.type !== "private") {
+    if (
+      !ctx.message.text.includes(botUserName) &&
+      !ctx.message.text.includes("/format")
+    )
+      return;
+  }
+
+  let msg = "";
+  let _user = {};
+
+  // check is the message is a reply
+  if (ctx.message.reply_to_message) {
+    logger.info(`Reply message recieved from ${ctx.message.from.username}`);
+    // if (!ctx.message.reply_to_message.from.is_bot) return;
+    if (!ctx.message.reply_to_message.text) return;
+    msg = ctx.message.reply_to_message.text
+      .replace(botUserName, "")
+      .replace("/format", "");
+    _user = ctx.message.reply_to_message.from;
+  } else {
+    logger.info(`Message recieved from ${ctx.message.from.username}`);
+    if (!ctx.message.text) return;
+    msg = ctx.message.text.replace(botUserName, "").replace("/format", "");
+    _user = ctx.message.from;
+  }
+  const codeImage = generateCodeImage(msg);
+  const avatar = getAvatar(ctx, _user.id);
+
+  const image = await getImage(await codeImage, {
+    name: _user.first_name + " " + (_user.last_name || ""),
+    username: _user.username ? `@${_user.username}` : _user.id,
+    avatar: await avatar,
+  });
+
+  ctx.replyWithPhoto(Input.fromBuffer(image, "code.png"), {
+    caption: "Here is your code 🎉",
   });
 });
 
@@ -25,3 +66,16 @@ bot.launch();
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
+async function getAvatar(ctx, id) {
+  try {
+    const userAvatar = await ctx.telegram.getUserProfilePhotos(id);
+    const avatarId = userAvatar.photos[0][0].file_id;
+    const avatar = await ctx.telegram.getFileLink(avatarId);
+    return (
+      avatar.toString() || "https://avatars.githubusercontent.com/u/124599?v=4"
+    );
+  } catch (error) {
+    return "https://avatars.githubusercontent.com/u/124599?v=4";
+  }
+}
